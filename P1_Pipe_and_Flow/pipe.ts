27b73e36fd6aee7cb86dc8385f875377ce0282f8
Christian Arana <christian.arana.ctr@one.app>
2022-07-18 19:35:01 -0600

    Imported functions in pipe script

--- P1_Pipe_and_Flow/pipe.ts
+++ P1_Pipe_and_Flow/pipe.ts
@@ -1,68 +1,8 @@
+// Import pipe from fp-ts
 import { pipe } from 'fp-ts/lib/function';
 
-// First create the functions to pipe them together
-
-/**
- * # Add 1
- * 
- * Add 1 to a number that is passed in
- * 
- * @param x 
- * @returns {number} value of x + 1
- */
-function add1(x: number): number {
-  return x + 1;
-}
-
-/**
- * # Multiply by 2
- * 
- * Multiply a number by 2
- * 
- * @param x 
- * @returns {number} value of x * 2
- */
-function multiply2(x: number): number {
-  return x * 2;
-}
-
-/**
- * # Number to String
- * 
- * Convert a number into a string
- * 
- * @param num 
- * @returns 
- */
-function numberToString(num: number): string {
-  return `${num}`
-}
-
-/**
- * # Log Value
- * 
- * Log a value to the console, and return it
- * 
- * @param value 
- * @returns 
- */
-function log(value: any): any {
-  console.log(value);
-  return value;
-}
-
-/**
- * # Evaluate value
- * 
- * Log a value to the console, and return the value
- * 
- * @param x 
- * @returns 
- */
-function typeEvaluator(x: any): any {
-  console.log(typeof x);
-  return x;
-}
+// Import functions to be used with pipe
+import { add1, log, multiply2, numberToString, typeEvaluator } from './functions';
 
 // Now pipe the functions together
 pipe(1, add1, multiply2, log, typeEvaluator); // 4
