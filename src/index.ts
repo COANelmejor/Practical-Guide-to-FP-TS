e46fc6602c9c2db8ffccdf472459573fb971a487
Christian Arana <christian.arana.ctr@one.app>
2022-07-21 11:42:07 -0600

    Updated placeholder script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // This script is a placeholder to make sure that the typescript compiler is working and the `fp-ts`
 // library is installed correctly.
-// It is an example of fp-ts usage of Array.
+// It is an example of fp-ts usage of Pipe and Array.
 
 import * as A from "fp-ts/Array";
 
@@ -8,6 +8,10 @@ import { pipe } from "fp-ts/function";
 
 pipe(
     [1, 2, 3, 4],
+    (value) => {
+        console.log("Installation is working correctly. (•◡•)");
+        return value;
+    },
     A.map((x) => x + 1),
-    console.log
+    console.log,
 );
